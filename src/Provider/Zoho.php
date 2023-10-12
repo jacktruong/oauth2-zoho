537ecd12efdb1d3f3598211b9d95489c587455e4
Jack <jack.truong@uwaterloo.ca>
2023-10-12 15:03:25 -0400

    Added Canadian DC

--- src/Provider/Zoho.php
+++ src/Provider/Zoho.php
@@ -25,6 +25,7 @@ class Zoho extends AbstractProvider
         'EU' => 'https://accounts.zoho.eu',
         'IN' => 'https://accounts.zoho.in',
         'CN' => 'https://accounts.zoho.com.cn',
+        'CA' => 'https://accounts.zohocloud.ca',
     ];
 
     /**
